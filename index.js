2ece731087795b8e4909a338836b1b952fe8709e
Kryptify <xinzhe1219@outlook.com>
2023-04-20 00:20:28 +0900

    initial commit

---
+++ index.js
@@ -0,0 +1,286 @@
+const fs = require('fs')
+const Web3 = require('web3');
+const axios = require('axios')
+
+const { privateKey, providerURL, nodeURL } = require("./secrets.json")
+const { contractAddress, abi, chainId, maxScanBlockCountForEvents, maxScanBlockCountForFee } = require("./config.json")
+const { logEvent, syncNonceForAccount, readInfo, saveInfo } = require('./utils.js')
+
+const web3 = new Web3(providerURL)
+
+const processEvents = async () => {
+  const info = readInfo()
+  const fromBlock = info.lastBlockForEvents + 1
+  const lastBlock = await web3.eth.getBlockNumber()
+
+  const toBlock = Math.min(fromBlock+maxScanBlockCountForEvents-1, lastBlock)
+
+  if (fromBlock > toBlock) {
+    return true
+  }
+
+  console.log(`Scanning block ${fromBlock} ~ ${toBlock} for Events...`)
+
+  const contract = new web3.eth.Contract(abi, contractAddress)
+  const options = {
+    filter: {},
+    fromBlock: fromBlock,
+    toBlock: toBlock
+  }
+
+  const events = await contract.getPastEvents("allEvents", options)
+
+  for (let i = 0; i < events.length; i++) {
+    const event = events[i];
+    logEvent(event);
+  }
+
+  saveInfo({...info, lastBlockForEvents: toBlock})
+  console.log("Scanning for Events finished")
+  return false
+}
+
+const distributeT1Reward = async () => {
+
+  console.log("Calling distributeT1Reward")
+
+  const account = web3.eth.accounts.privateKeyToAccount(privateKey)
+  const contract = new web3.eth.Contract(abi, contractAddress)
+  const transaction = contract.methods.distributeT1Reward()
+  const encodedABI = transaction.encodeABI()
+  const estimatedGas = await transaction.estimateGas({from: account.address})
+  const gasPrice = await web3.eth.getGasPrice()
+
+  const info = readInfo()
+  const nonce = info.nonce+1
+  saveInfo({...info, nonce})
+
+  const options = {
+    // nonce: nonce,
+    chainId: chainId.toString(),
+    to: transaction._parent._address,
+    gas: estimatedGas * 2,
+    gasPrice: gasPrice*2,
+    data: encodedABI
+  }
+
+  const signed = await web3.eth.accounts.signTransaction(options, privateKey)
+
+  try {
+    await web3.eth.sendSignedTransaction(signed.rawTransaction)
+  }
+  catch (error) {
+    console.log(`distributeT1Reward error: " + ${error}`)
+  }
+}
+
+const processT1Reward = async () => {
+  // check if there is any T1 licensor to reard
+  const contract = new web3.eth.Contract(abi, contractAddress)
+  let result = await contract.methods.shouldDistributeT1Reward().call();
+  if (result == false) {
+    console.log("No T1 Licensor to reward. Skipping...")
+    return
+  }
+
+  // call distributeT1Reward if there is any
+  await distributeT1Reward()
+}
+
+const getBlockFeeReward = async (blockNo) => {
+  let txFeeHex = 0
+  try {
+    axios.defaults.baseURL = nodeURL
+    const payload = {
+      "jsonrpc": "2.0",
+      "method": "eth_getMinerDataByBlockNumber",
+      "params": [`${blockNo}`],
+      "id": 1
+    }
+    let res = await axios.post("/", payload)
+    txFeeHex = res.data.result.transactionFee
+    let txFee = web3.utils.hexToNumber(txFeeHex, true)
+    return txFee
+  } catch (error) {
+    console.log("getBlockReward error: ", error)
+    console.log({blockNo, txFeeHex})
+    return 0
+  }
+}
+
+const distributeT2Reward = async (rewardFeeArray, lastBlockNo) => {
+  console.log("Calling distributeT2Reward...")
+  let blockNos = []
+  let blockFees = []
+  for (let i = 0; i < rewardFeeArray.length; i++) {
+    blockNos.push(rewardFeeArray[i].blockNo)
+    blockFees.push(rewardFeeArray[i].blockFee)
+  }
+
+  console.log({blockNos})
+  console.log({blockFees})
+  console.log({lastBlockNo})
+
+  const account = web3.eth.accounts.privateKeyToAccount(privateKey)
+  const contract = new web3.eth.Contract(abi, contractAddress)
+  const transaction = contract.methods.distributeT2Reward(blockNos, blockFees, lastBlockNo)
+  const encodedABI = transaction.encodeABI()
+  const estimatedGas = await transaction.estimateGas({from: account.address})
+  const gasPrice = await web3.eth.getGasPrice()
+  
+  const info = readInfo()
+  const nonce = info.nonce+1
+  saveInfo({...info, nonce})
+
+  const options = {
+    // nonce: nonce,
+    chainId: chainId.toString(),
+    to: transaction._parent._address,
+    gas: estimatedGas * 2,
+    gasPrice: gasPrice*2,
+    data: encodedABI
+  }
+
+  const signed = await web3.eth.accounts.signTransaction(options, privateKey)
+
+  try {
+    await web3.eth.sendSignedTransaction(signed.rawTransaction)
+    return true
+  }
+  catch (error) {
+    console.log(`distributeT2Reward error: " + ${error}`)
+    return false
+  }
+}
+
+const processT2Reward = async () => {
+  try {
+    const info = readInfo()
+    const fromBlock = info.lastBlockForFee + 1
+    const lastBlock = await web3.eth.getBlockNumber()
+    const toBlock = Math.min(fromBlock+maxScanBlockCountForFee-1, lastBlock)
+
+    if (fromBlock > toBlock) {
+      return true
+    }
+
+    // check if there T2 licensors
+    // if there is no one we don't need to do it
+    const contract = new web3.eth.Contract(abi, contractAddress)
+    let result = await contract.methods.t2LicenseCount().call();
+    if (result == 0) {
+      console.log("No T2 Licensor. Skipping...")
+      saveInfo({...info, lastBlockForFee: toBlock})
+      return true
+    }
+
+    console.log(`Scanning block ${fromBlock} ~ ${toBlock} for Fee Rewards...`)
+
+    let blockFeeArray = []
+    for (let blockNo = fromBlock; blockNo <= toBlock; blockNo++) {
+      let blockFee = await getBlockFeeReward(blockNo)
+      let blockFeeReward = {blockNo, blockFee}
+      blockFeeArray.push(blockFeeReward)
+    }
+
+    // distribute T2 rewards
+    if (blockFeeArray.length == 0) {
+      return false;
+    }
+
+    let distributed = await distributeT2Reward(blockFeeArray, toBlock)
+    if (distributed == false) {
+      return false;
+    }
+
+    saveInfo({...info, lastBlockForFee: toBlock})
+    console.log("Scanning for Fee Rewards finished")
+
+    if (fromBlock >= toBlock-1) {
+      return true
+    }
+    else {
+      return false
+    }
+  } catch (err) {
+    console.log("Scanning for Fee Rewards error:", err)
+    return false
+  }
+}
+
+const processBurnSurplusCoins = async () => {
+  let date = new Date()
+  let hours = date.getHours()
+  let mins = date.getMinutes()
+
+  if (hours < 23 || mins < 30) {
+    return;
+  }
+
+  const contract = new web3.eth.Contract(abi, contractAddress)
+  let result = await contract.methods.shouldBurnSurplusCoins().call()
+  if (result == false) {
+    return;
+  }
+
+  console.log(`Calling processBurnSurplusCoins...`)
+
+  const account = web3.eth.accounts.privateKeyToAccount(privateKey)
+  const transaction = contract.methods.burnSurplusCoins()
+  const encodedABI = transaction.encodeABI()
+  const estimatedGas = await transaction.estimateGas({from: account.address})
+  const gasPrice = await web3.eth.getGasPrice()
+  
+  const info = readInfo()
+  const nonce = info.nonce+1
+  saveInfo({...info, nonce})
+
+  const options = {
+    // nonce: nonce,
+    chainId: chainId.toString(),
+    to: transaction._parent._address,
+    gas: estimatedGas*2,
+    gasPrice: gasPrice*2,
+    data: encodedABI
+  }
+
+  const signed = await web3.eth.accounts.signTransaction(options, privateKey)
+
+  try {
+    await web3.eth.sendSignedTransaction(signed.rawTransaction)
+    return true
+  }
+  catch (error) {
+    console.log(`processBurnSurplusCoins error: " + ${error}`)
+    return false
+  }
+}
+
+async function main() {
+  // scan license purchase
+  let scannedToEnd = false
+  while (scannedToEnd == false) {
+     scannedToEnd = await processEvents()
+  }
+
+  // reward distribution for T1 licensors
+  await processT1Reward()
+
+  // reward distribution for T2 licensors
+  scannedToEnd = false
+  while (scannedToEnd == false) {
+    scannedToEnd = await processT2Reward()
+  }
+
+  // process surplus coin burning
+  await processBurnSurplusCoins()
+
+  setTimeout(async () => {
+    await main()
+  }, 10000)
+}
+
+syncNonceForAccount()
+  .then(async () => {
+    await main()
+  })
---
+++ utils.js
@@ -0,0 +1,82 @@
+const fs = require('fs')
+const Web3 = require('web3');
+
+const { privateKey, providerURL } = require("./secrets.json")
+
+const web3 = new Web3(providerURL)
+
+const defaultInfo = {
+  // nonce: 0,
+  lastBlockForReceived: -1
+}
+
+const saveInfo = (info) => {
+  const data = JSON.stringify(info)
+  try {
+    fs.writeFileSync('info.dat', data)
+  }
+  catch (err) {
+    console.log(`Error saving info file: ${err}`)
+  }
+}
+
+const readInfo = () => {
+  try {
+    const data = fs.readFileSync('info.dat', 'utf-8')
+    const info = JSON.parse(data)
+    return info
+  }
+  catch (err) {
+    console.log(`Error reading info file: ${err}`)
+    return defaultInfo
+  }
+}
+
+const syncNonceForAccount = async () => {
+  console.log("synchronizing nonce...")
+  try {
+    const account = web3.eth.accounts.privateKeyToAccount(privateKey)
+    const nonce = await web3.eth.getTransactionCount(account.address)
+    const info = readInfo()
+    saveInfo({...info, nonce})
+    console.log("synchronized. nonce is " + nonce)
+    return nonce
+  }
+  catch (err) {
+    console.log(err)
+    return 0
+  }
+}
+
+const checkAmountMatchLicenseFormat = (amountInWei) => {
+  const amountInEther = web3.utils.fromWei(amountInWei, 'ether')
+  const amountInEtherString = amountInEther.toString()
+
+  const decimalIndex = amountInEtherString.indexOf('.')
+  const decimalCount = decimalIndex == -1 ? 0 : amountInEtherString.length - decimalIndex - 1
+  if (decimalCount !== 3) {
+    return null
+  }
+
+  const decimalString = amountInEtherString.substring(decimalIndex+1, decimalIndex+decimalCount+1)
+  if (decimalString === "001") {
+    return "T1"
+  }
+  else if (decimalString == "003") {
+    return "T2"
+  }
+  else {
+    return null
+  }
+}
+
+const logEvent = (event) => {
+  let eventLog = {
+    event: event.event,
+    blockNumber: event.blockNumber,
+    values: event.returnValues
+  }
+  console.log(eventLog)
+}
+
+module.exports = { saveInfo, readInfo, logEvent, syncNonceForAccount, checkAmountMatchLicenseFormat };
